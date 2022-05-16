589f6c7daf2c3b27eaca55c1810ecd2d7156d9a0
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2022-05-16 14:26:16 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,43 +1,12 @@
 $(document).ready(function() {
-    // new WOW().init();
+    new WOW().init();
     $('[data-toggle="tooltip"]').tooltip();
 
     $(".navbar-toggler").click(function() {
         $('.helper-task').toggleClass('overflow')
     })
 
-    $(".scrolltop").hide();
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('.scrolltop').fadeIn();
-        } else {
-            $('.scrolltop').fadeOut();
-        }
-    });
-    $('.scrolltop').click(function() {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 800);
-    });
 
-})
-
-$(window).scroll(function() {
-    var nav = $('.bg-scroll');
-    var top = 20;
-    if ($(window).scrollTop() >= top) {
-
-        nav.addClass('bg-black');
-
-    } else {
-        nav.removeClass('bg-black');
-    }
-});
-
-$(".chevron-wrapper").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#first").offset().top - 0
-    }, 500);
 })
 
 $('.top-slider').slick({
@@ -78,40 +47,11 @@ $('.top-slider').slick({
     ]
 });
 
-$('.campaign').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    adaptiveHeight: true,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                centerPadding: '60px',
-                adaptiveHeight: true
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
+function showHide() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
